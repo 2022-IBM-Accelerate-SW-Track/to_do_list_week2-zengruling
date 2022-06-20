406d156bc7d76f4939ff2c87468a833e1e996612
zeng28 <zeng28@illinois.edu>
2022-06-19 23:50:25 -0500

    Completed Todo's checkbox feature.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import profile from "../assets/profile.jpg";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
         <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
+      <div className="centered">
+        <img 
+          className="profile"
+          src={profile}
+          alt="profile image"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Hi, I'm Carolyn Zeng.</div>
+        <div className="brief_description">
+          I'm a rising junior majoring in Computer Science + Astronomy at the University of Illinois at Urbana-Champaign (UIUC). Glad to be in this program with everybody!
+          My interests include AI, video games, quantum physics, anime/manga, and DND. My favorite band is Lord Huron.
+          Feel free to connect on <a href="https://www.linkedin.com/in/carolyn-z-28637718b/">LinkedIn</a> or contact me on Discord at kaedezee#5636.
         </div>
       </div>
-    );
+    </div>
+      </div>
+    )
   }
 }
